c26fcbe20a4dbdc0d421acd492647469b377585e
aurimas-darguzis <aurimas@darguzis.com>
2017-05-11 21:02:53 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Aurimas Darguzis';
+}
+
+
+// start on material design
+// https://medium.com/@ladyleet/setting-up-your-first-angular-2-project-using-angular-material2-5db18a174165
