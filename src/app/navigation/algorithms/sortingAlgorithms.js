374d11b664ef21501523e4bdfdecfc949289e89b
aurimas-darguzis <aurimas@darguzis.com>
2017-05-13 11:43:01 +0100

    merge algo

--- src/app/navigation/algorithms/sortingAlgorithms.js
+++ src/app/navigation/algorithms/sortingAlgorithms.js
@@ -1,3 +1,5 @@
+// http://btholt.github.io/four-semesters-of-cs/
+
 insertionSort = (nums) => {
   for (let i = 1; i < nums.length; i++) {
     for (let j = 0; j < i; j++) {
@@ -10,3 +12,18 @@ insertionSort = (nums) => {
     }
   }
 }
+
+// Merge sort
+
+/* 
+  it uses recursion, useful and consistent algorithm.
+  The basic gist of merge sort is that you are going to take your
+  big list, and first divide down in two half size lists and
+  recursively call merge sort on those smaller list, which in turn
+  will do the same. The base case is when you have a list of one, 
+  at which point you will return that sorted list of one.
+  On the way up the recursive calls, you will merge those sorted
+  lists together (preferably by another merge function you will write)
+  that walks through both lists simultaneously and insert the smaller
+  value first, effectively creating a bigger sorted list. 
+*/
