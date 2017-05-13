20a63a9ff0b67c3c384d1e39d791780c4768fd59
aurimas-darguzis <aurimas@darguzis.com>
2017-05-13 12:27:02 +0100

    added unit test for mergeSort algorthm

--- src/app/navigation/algorithms/sortingAlgorithms.js
+++ src/app/navigation/algorithms/sortingAlgorithms.js
@@ -76,3 +76,11 @@ const stitch = (left, right) => {
   return results
 }
 
+// unit test for mergeSort
+describe('insertion sort', function() {
+  it('should sort correctly', () => {
+    const nums = [10,5,3,8,2,6,4,7,9,1]
+    const ans = mergeSort(nums)
+    expect(ans).toEqual([1,2,3,4,5,6,7,8,9,10])
+  })
+})
