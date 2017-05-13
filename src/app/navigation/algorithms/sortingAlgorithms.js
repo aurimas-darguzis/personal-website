9c30e6bf855ce19ad855bff70c66d2cb118935b6
aurimas-darguzis <aurimas@darguzis.com>
2017-05-13 12:25:09 +0100

    implemented mergeSort algorithm

--- src/app/navigation/algorithms/sortingAlgorithms.js
+++ src/app/navigation/algorithms/sortingAlgorithms.js
@@ -27,3 +27,52 @@ insertionSort = (nums) => {
   that walks through both lists simultaneously and insert the smaller
   value first, effectively creating a bigger sorted list. 
 */
+
+const mergeSort = (nums) => {
+  // first thing you do when using recursive functions
+  // is to write if statement to break out of the funciton
+  if (nums.length < 2) { // if the length is 1, than the array is sorted
+    return nums
+  }
+
+  const length = nums.length
+  // always round down to the nereast number
+  const middle = Math.floor(length / 2)
+
+  // let's split our array into two smaller arrays
+  // slice is an array function (not destructive)
+  const left = nums.slice(0, middle)
+  // its going to go from middle to the end of the array
+  const right = nums.slice(middle, length)
+
+  // recursive call on both sides
+  const sortedLeft = mergeSort(left)
+  const sortedRight = mergeSort(right)
+  // and stitch sorted arrays together
+  return stitch(sortedLeft, sortedRight)
+}
+
+const stitch = (left, right) => {
+  // inner array that we are going to return in the end
+  const results = []
+
+  while(left.length && right.length) {
+    if (left[0] <= right[0]) {
+      // take my element and put it at the end - that's what .push do'
+      // shift is going to take first or zero element and take the off, and push into results
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+
+  while(left.length) {
+    results.push(left.shift())
+  }
+  while(right.length) {
+    results.push(right.shift())
+  }
+
+  return results
+}
+
