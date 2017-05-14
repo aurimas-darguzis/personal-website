4ec425894091481b3b29171a5389883e0a1420a6
aurimas-darguzis <aurimas@darguzis.com>
2017-05-14 18:33:12 +0100

    building tile compoennt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Aurimas Darguzis';
 
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-  ];
 }
 
 
--- src/app/showreel/showreel.component.ts
+++ src/app/showreel/showreel.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showreel.component.css']
 })
 export class ShowreelComponent implements OnInit {
+  tiles = [
+    {text: 'Angular',   cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'React',   cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Vue',   cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Node',   cols: 1, rows: 1, color: 'lightblue'},
+    // {text: 'One',   cols: 3, rows: 1, color: 'lightblue'},
+    // {text: 'Two',   cols: 1, rows: 2, color: 'lightgreen'},
+    // {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    // {text: 'Four',  cols: 1, rows: 1, color: '#DDBDF1'},
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
