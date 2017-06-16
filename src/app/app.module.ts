a73f819fc47f7e1bf14bebe0d8f2de3871803e49
aurimas-darguzis <aurimas@darguzis.com>
2017-06-16 13:59:14 +0100

    setted up routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,12 @@ import { SigninComponent } from './auth/signin/signin.component';
 
 // firebaseconfig
 export const firebaseConfig = {
-
+    apiKey: 'AIzaSyAaUrcQV4PZObFU61mZXjsK39Dce2vC5Zw',
+    authDomain: 'civic-genre-139017.firebaseapp.com',
+    databaseURL: 'https://civic-genre-139017.firebaseio.com',
+    projectId: 'civic-genre-139017',
+    storageBucket: 'civic-genre-139017.appspot.com',
+    messagingSenderId: '644038543495'
 };
 
 @NgModule({
