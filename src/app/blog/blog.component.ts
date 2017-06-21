d89cfedcc938eea5021cd7158d5462e74f00f675
aurimas-darguzis <aurimas@darguzis.com>
2017-06-21 23:44:05 +0100

    putting down an image. WIP: animated message

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
-// get the images
+
+// private image_1 = '//src/app/blog/img/1.png';
+// private image_2 = '//src/app/blog/img/2.png';
+// private image_3 = '//src/app/blog/img/3.png';
+// private image_4 = '//src/app/blog/img/4.png';
+// private image_5 = '//src/app/blog/img/5.png';
+// private image_6 = '//src/app/blog/img/6.png';
+// private image_7 = './img/7.png';
+// private image_8 = './img/8.png';
+// private image_9 = './img/9.png';
+// private image_10 = './img/10.png';
+// private image_11 = './img/11.png';
+// private image_12 = './img/12.png';
+// private image_13 = './img/13.png';
+// private image_14 = './img/14.png';
   constructor() { }
 
   ngOnInit() {
