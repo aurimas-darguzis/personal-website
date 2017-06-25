9bbc8cb5dd4a2a915426e864d185bb4d80ca73eb
aurimas-darguzis <aurimas@darguzis.com>
2017-06-25 22:25:44 +0100

    details, 2 links for editing is set to work

--- src/app/showreel/showreel-project/project-edit/project-edit.component.ts
+++ src/app/showreel/showreel-project/project-edit/project-edit.component.ts
@@ -1,5 +1,7 @@
+import { ShowreelService } from './../../showreel.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Project } from '../../showreel.model';
 
 @Component({
   selector: 'app-project-edit',
@@ -7,10 +9,12 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./project-edit.component.css']
 })
 export class ProjectEditComponent implements OnInit {
+  project: Project;
   id: number;
   editMode = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private showreelService: ShowreelService) { }
 
   ngOnInit() {
     this.route.params
@@ -19,7 +23,10 @@ export class ProjectEditComponent implements OnInit {
           this.id = +params['id'];
           // check does it have an id
           this.editMode = params['id'] != null;
+          this.project = this.showreelService.getProject(this.id);
         }
       );
   }
+
+
 }
--- src/app/showreel/showreel-project/showreel-project.component.ts
+++ src/app/showreel/showreel-project/showreel-project.component.ts
@@ -38,9 +38,9 @@ export class ShowreelProjectComponent implements OnInit {
     return;
   }
 
-  onEditProject (id) {
-    console.log(id);
-    this.router.navigate([`edit/${id}`], {relativeTo: this.route});
+  // onEditProject (id) {
+  //   console.log(id);
+    // this.router.navigate([`${id}/edit`], {relativeTo: this.route});
     // this.router.navigate(['../', this.id, 'edit']);
-  }
+  // }
 }
