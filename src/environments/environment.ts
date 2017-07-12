cfcf3730c25628d04757f0548eee38079e74361e
Aurimas <aurimas@darguzis.com>
2017-07-12 16:39:55 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,5 @@
 
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyAaUrcQV4PZObFU61mZXjsK39Dce2vC5Zw',
-    authDomain: 'civic-genre-139017.firebaseapp.com',
-    databaseURL: 'https://civic-genre-139017.firebaseio.com',
-    projectId: 'civic-genre-139017',
-    storageBucket: 'civic-genre-139017.appspot.com',
-    messagingSenderId: '644038543495'
-  }
+
 };
