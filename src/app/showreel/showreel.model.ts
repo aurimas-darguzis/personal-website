0101595ca7b44e8ae6962f772d4431d1410b36b3
aurimas-darguzis <aurimas@darguzis.com>
2017-07-12 11:52:48 +0100

    updated showreel model structure. From interface to class object

--- src/app/showreel/showreel.model.ts
+++ src/app/showreel/showreel.model.ts
@@ -1,7 +1,8 @@
-export  interface Project {
-    id: number;
+export  class Project {
+    $key: string;
     name: string;
     description: string;
     imagePath: string;
     category: string;
+    timeStamp: Date = new Date();
   }
