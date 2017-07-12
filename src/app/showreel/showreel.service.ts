803e3312266244cb99d2e961873460f0aabcdf20
aurimas-darguzis <aurimas@darguzis.com>
2017-07-12 12:20:22 +0100

    implemented firebase methods for crud in service

--- src/app/showreel/showreel.service.ts
+++ src/app/showreel/showreel.service.ts
@@ -7,9 +7,56 @@ export class ShowreelService {
 
   private basePath = '/projects';
 
-  constructor() { }
+  projects: FirebaseListObservable<Project[]> = null; // list of projects
+  project: FirebaseObjectObservable<Project> = null; // single project
+
+  constructor(private db: AngularFireDatabase) { }
+
+  // Return an observable list with optional query
+  // You will usually call this from OnInit in a component
+  getProjectsList (query = {}): FirebaseListObservable<Project[]> {
+    this.projects = this.db.list('/projects', {
+      query: query
+    });
+    return this.projects;
+  }
+
+  // Return a single observable item
+  getProject (key: string): FirebaseObjectObservable<Project> {
+    const projectPath = `${this.basePath}/${key}`;
+    this.project = this.db.object(projectPath);
+    return this.project;
+  }
+
+  // Create new project
+  createProject (project: Project): void {
+    this.projects.push(project)
+      .catch(error => this.handleError(error));
+  }
+
+  // Update an existing project
+  updateProject (key: string, value: any): void {
+    this.projects.update(key, value)
+      .catch(error => this.handleError(error));
+  }
+
+  // Delete a single project
+  deleteProject (key: string): void {
+    this.projects.remove(key)
+      .catch(error => this.handleError(error));
+  }
+
+  // Delete all projects
+  deleteAll (): void {
+    this.projects.remove()
+      .catch(error => this.handleError(error));
+  }
 
 
+  // Default error handling for all actions
+  private handleError (error) {
+    console.error(error);
+  }
 
   /* ::: Firebase playground :::
 
