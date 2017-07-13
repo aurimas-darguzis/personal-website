a8ce5f3354c6827681f70d6da354a978f7b724f0
aurimas-darguzis <aurimas@darguzis.com>
2017-07-13 19:21:23 +0100

    providing firebase auth module into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +9,3 @@ export class AppComponent {
   title = 'Aurimas Darguzis';
 
 }
-
-
-// start on material design
-// https://medium.com/@ladyleet/setting-up-your-first-angular-2-project-using-angular-material2-5db18a174165
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-// import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 // import { AF } from './providers/af';
 
@@ -62,7 +62,7 @@ import { HomeComponent } from './home/home.component';
     FlexLayoutModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    // AngularFireAuthModule
+    AngularFireAuthModule
   ],
   providers: [ShowreelService, ProjectsService],
   bootstrap: [AppComponent]
