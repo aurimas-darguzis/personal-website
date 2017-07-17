4f012aaac697316aff7d5ae59c7606523e2995b7
aurimas-darguzis <aurimas@darguzis.com>
2017-07-17 12:28:54 +0100

    5 tests passing, 12 to go

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
+import { MaterialModule } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import {} from 'jasmine';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        RouterTestingModule,
+        MaterialModule
       ],
+      declarations: [
+        AppComponent,
+        HeaderComponent
+      ]
     }).compileComponents();
   }));
 
