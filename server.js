b46016ea12b85cac85376999ef1a91246457ab4e
aurimas-darguzis <aurimas@darguzis.com>
2017-12-23 16:33:08 +0000

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const port = process.env.PORT || 3030;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const env = process.env.NODE_ENV || 'development';
+const mongo = ''
+
