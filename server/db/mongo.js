cbc0e7eb1f9b98ae7c5a1d9ec7ec7c55422f8d56
aurimas-darguzis <aurimas@darguzis.com>
2017-12-23 17:29:59 +0000

    update

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const connect = () => {
+  return new Promsie((resolve ,reject) => {
+    mongoose.connect(process.env.MONGO_URI, {useMongoClient: true}, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const someSchema = new Schema({
+  id: String,
+  title: String,
+  lastModified: Date
+});
