7da24a8fd31185ce7147a1c5352bd5452009a7a6
aurimas-darguzis <aurimas@darguzis.com>
2017-12-24 01:27:18 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { HomeComponent } from './components/home/home.component';
+import { MeComponent } from './components/me/me.component';
+import { ShowreelComponent } from './components/showreel/showreel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MeComponent,
+    ShowreelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/showreel/showreel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showreel',
+  templateUrl: './showreel.component.html',
+  styleUrls: ['./showreel.component.css']
+})
+export class ShowreelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
